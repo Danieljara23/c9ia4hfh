9bc46eeafd96063b082aded875c86f752f51809e
Jara2392 <daniel.jaramillog@udea.edu.co>
2017-02-17 12:13:46 -0500

    delete action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -17,19 +17,31 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+      redirect_to root_path
+  end
+
+  def update
+    if @room.update(room_params)
+      flash[:notice] = 'Room was successfully updated'
+      redirect_to root_path
+    else
+      render edit
+    end
+  end
+
   def destroy
     @room.destroy
     flash[:notice] = 'Room was successfully deleted.'
     redirect_to rooms_path
   end
 
-
   protected
     def room_params
       params.require(:room).permit(:title, :description, :beds,:guests, :price_per_night, :image_url)
     end
-
     def find_room
-    @room = Room.find(params[:id])
-  end
+      @room = Room.find(params[:id])
+    end
+
 end
